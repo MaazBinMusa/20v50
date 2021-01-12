ca99cf6861fd841272ff581927eb0525a2319748
Musa <maazbinmusa94@gmail.com>
2021-01-12 15:33:31 +0500

    graph created

--- flask/vizualize.py
+++ flask/vizualize.py
@@ -3,11 +3,13 @@ import json
 import csv
 from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
 
 data = []
 count = {}
 
-with open('run2_2020-12-28_final.json','r') as file:
+with open('run3_2020-12-30_final.json','r') as file:
 	data = json.load(file)
 
 for item in data:
@@ -26,7 +28,43 @@ for item in data:
 	count[num][cat].append(hash_)
 
 
-for k in [5,10,15,20,25,50,75,100]:
+UniqueAds = []
+PersonaSize = [5,10,15,20,25,50,75,100]
+
+for k in PersonaSize:
 	print(k)
 	print(len(set(count[k]['All'])))
 	print("-----------")
+	UniqueAds.append(len(set(count[k]['All'])))
+
+
+data = {'PersonaSize':PersonaSize,'UniqueAds':UniqueAds}
+df = pd.DataFrame(data)
+
+# fig1 = sns.barplot(x='PersonaSize', y='UniqueAds', data = df, palette='summer')
+# fig1.figure.savefig("check1.png")
+fig2 = sns.lineplot(x='PersonaSize', y='UniqueAds', data = df, color='tab:red')
+fig2.figure.savefig("check2.png")
+
+# fig, ax1 = plt.subplots(figsize=(10,6))
+# color = 'tab:green'
+
+# #bar plot creation
+# ax1.set_title('Unique Ads by Persona Size', fontsize=16)
+# ax1.set_xlabel('Persona Size', fontsize=16)
+# ax1.set_ylabel('Unique Ads', fontsize=16)
+# ax1 = sns.barplot(x='PersonaSize', y='UniqueAds', data = df, palette='summer')
+# ax1.tick_params(axis='y')
+
+# #specify we want to share the same x-axis
+# ax2 = ax1.twinx()
+# color = 'tab:red'
+
+# #line plot creation
+# ax2 = sns.lineplot(x='PersonaSize', y='UniqueAds', data = df, color=color)
+# ax2.tick_params(axis='y', color=color)
+
+# #show plot
+# print("Plt")
+# plt.show()
+# plt.savefig("check.png")
